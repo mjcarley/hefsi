34461da4777e9e9b83c49a21c7f249d6a4af8bc3
Michael Carley <michael@paraffinalia.co.uk>
2023-05-22 11:30:27 +0100

    doc: added GPL notice and reference to Moller's triangle-triangle intersection code

--- hefsi.h
+++ hefsi.h
@@ -2,12 +2,12 @@
  *
  * Copyright (C) 2023 Michael Carley
  *
- * AGG is free software: you can redistribute it and/or modify it
+ * HEFSI is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.  AGG is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * (at your option) any later version.  HEFSI is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
